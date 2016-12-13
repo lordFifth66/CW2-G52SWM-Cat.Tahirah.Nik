51559db18b5c1688aaf0f5ad650576d62f808272
zaquan1 <amirulzaquan@yahoo.com>
2016-12-13 13:44:28 +0800

    Link The Map Viewer with the game

--- Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
@@ -1,6 +1,27 @@
 package com.neet.DiamondHunter.JavaFX;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.neet.DiamondHunter.Main.Game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 //this class contains Controller for JavaFX
 public class Controller {
+	@FXML
+    private ResourceBundle resources;
 
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button runGame;
+
+    @FXML
+    void initialize() {
+    	runGame.setOnAction((event) ->{
+    		Game.Run();
+    	});
+    }
 }
