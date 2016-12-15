06227c3d693bbc63f1f701315ffeece514582e1b
zaquan1 <amirulzaquan@yahoo.com>
2016-12-16 01:25:08 +0800

    Fixing merge conflict
    -really having a hard time understanding all of this conflict
    
    -still a newb here, sorry

--- Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
@@ -12,6 +12,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 //this class contains Controller for JavaFX
@@ -33,6 +34,19 @@ public class Controller {
 
     @FXML
     private Label yCoordinate;
+    
+    @FXML
+    private TextField axeFieldX;
+ 
+    @FXML 
+    private TextField axeFieldY;
+ 
+    @FXML
+    private TextField boatFieldX;
+
+    @FXML 
+    private TextField boatFieldY;
+
     //create map
     private Map maps;
     private Objects items;
@@ -121,6 +135,12 @@ public class Controller {
     {
 		maps.draw(gc);
 		items.draw(gc);
+
+	    axeFieldX.setText(Integer.toString(Objects.axeX));
+	    axeFieldY.setText(Integer.toString(Objects.axeY));
+	    boatFieldX.setText(Integer.toString(Objects.boatX));
+	    boatFieldY.setText(Integer.toString(Objects.boatY));
+	 
     }
     
 }
