e85d4a5610f4d86a0bf61e2efc2ace3d9403c07d
zaquan1 <amirulzaquan@yahoo.com>
2016-12-19 17:43:57 +0800

    Adding constraint to the choices of possible axe and boat position
    -warning if user try to place axe or boat in a position that is
    impossible to reach

--- Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
+++ Diamond Hunter/src/com/neet/DiamondHunter/JavaFX/Controller.java
@@ -63,7 +63,7 @@ public class Controller {
     private int ylocation;
     
     //item coordinate
-    public static int itemStatus;
+    private int itemStatus;
 
 
     @FXML
@@ -125,18 +125,24 @@ public class Controller {
     		public void handle (MouseEvent mouseEvent) {
     			
     				if (checkTiles(xlocation, ylocation))
-    				{
+    				{	
     					if (itemStatus == 0)
     					{
     						Objects.axeX = (int)(mouseEvent.getX() / 16);
     						Objects.axeY = (int)(mouseEvent.getY() / 16);
     					}
-    					else
+    					else if(itemStatus == 1)
     					{
     						Objects.boatX = (int)(mouseEvent.getX() / 16);
     						Objects.boatY = (int)(mouseEvent.getY() / 16);	
     					}
-    					draw(gc);    					
+    					draw(gc);
+    					
+    					if (!(checkAxe(Objects.axeX, Objects.axeY) &&
+    							checkBoat(Objects.boatX, Objects.boatY)))
+    					{
+    						System.out.println("warning");
+    					}
     				}
     				else
     				{
@@ -144,14 +150,13 @@ public class Controller {
     					Alert alert = new Alert(AlertType.ERROR);
     					alert.setTitle("Error");
     					
-    					
     					if(itemStatus == 0)
     					{
     						alert.setHeaderText("Invalid Axe Location!");
         					alert.setContentText("The valid location for AXE is neither on any tree-like tile nor any blue-coloured tile (Make sure to position the AXE where you can possbily reach them).  ");
     					}
     					
-    					else
+    					else if(itemStatus == 1)
     					{
     						alert.setHeaderText("Invalid Boat Location!");
         					alert.setContentText("The valid location for BOAT is neither on any tree-like tile nor any blue-coloured tile (Make sure to position the BOAT where you can possbily reach them). ");
@@ -198,6 +203,31 @@ public class Controller {
 	    boatFieldX.setText(Integer.toString(Objects.boatX));
 	    boatFieldY.setText(Integer.toString(Objects.boatY));
 	 
+    }
+    private boolean checkAxe(int x, int y)
+    {
+    	if (y > 31) return false;
+    	if (x < 12)	return false;
+    	if (y < 8) return false;
+    	if ((x == 14 || x == 15) && y == 20) return false;
+    	if (y < 18 && x < 28)
+    	{
+    		if (y == 17 && (x < 23 && x > 16))
+    		{
+    			return true;
+    		}
+    		return false;
+    	}
+    	return true;
+    }
+    private boolean checkBoat(int x, int y)
+    {
+    	if (y < 8)
+    	{
+    		if (x < 13 || x > 27) return false;
+    	}
+    	if (x > 33 && y > 36) return false;
+    	return true;    	
     }
     
 }
